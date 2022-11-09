0cd326c076c9c9e2549cb6bb5e9f124b2c46a017
Kante Mahamadou <mkantem@yahoo.fr>
2022-11-09 11:34:55 +0000

    Add files via upload

---
+++ Operateurs/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  //Operateurs arithmetiques
+
+   /* int compteur = 5;
+
+    compteur++;
+    cout << compteur <<endl;
+    cout << compteur++ <<endl;
+    cout << ++compteur <<endl; */
+
+	//Opérateurs arithmetiques
+    int a=5, b=5;
+
+    cout << !(a==5 || b==5) ;
+
+  /* Opérateurs d'affectation
+    a += 2;
+    a = a +2;
+    a /= 2;
+    a %= 2;
+    a *= 2;  */
+
+    // Operateurs logiques &&, ||, !
+
+    // Operateurs relationnel >, <, >=, <=, ==, !=
+
+    return 0;
+}
